e6a2eb90dfceccc518655678d3880517cc969198
Quinn Bruderer <quintinbruderer@gmail.com>
2017-03-23 13:18:43 -0600

    Latest and Greatest

--- Project 2/ghost.js
+++ Project 2/ghost.js
@@ -1,10 +1,10 @@
-function Ghosts(x, y, color) {
+function Ghost(x, y, color) {
     this.ImIT = false;
-    this.pos = createVector(x,y);
+    this.pos = createVector(x, y);
     this.size = 70;
     this.gCol = color;
     this.eyes = this.size / 4;
-    this.velocity = createVector(0,0);
+    this.velocity = createVector(0, 0);
 
     this.display = function() {
         push();
@@ -34,22 +34,136 @@ function Ghosts(x, y, color) {
         pop();
     }
 
+    this.walls = function() {
+        if (this.pos.x - (this.size / 2) <= 0) {
+            this.pos.x = (this.size / 2) + 1;
+        } else if (this.pos.x + (this.size / 2) >= width) {
+            this.pos.x = width - ((this.size / 2) - 1);
+        } else if (this.pos.y + 45 >= height) {
+            this.pos.y = height - 46;
+        } else if (this.pos.y - (this.size / 2) <= 0) {
+            this.pos.y = (this.size / 2) + 1;
+        }
+    }
 
+    this.moveA = function(ghost1, ghost2) {
+        this.run(ghost1, ghost2);
+        this.attackA(ghost1, ghost2);
+    }
+    this.moveB = function(ghost1, ghost2) {
+        this.run(ghost1, ghost2);
+        this.attackB(ghost1);
+    }
+    this.moveC = function(ghost1, ghost2) {
+        this.run(ghost1, ghost2);
+        //this.attackC(ghost1, ghost2);
+    }
 
-    this.movement = function(ghosts){
-
-      for (var i = 0; i < ghosts.length; i++) {
-              var test = ghosts[i];
-              var ghostDist = p5.Vector.dist(test.pos,this.pos);
-      }
-
-
-      if (!this.ImIT){
+    this.run = function(ghost1, ghost2) {
+        var dist1 = this.pos.dist(ghost1.pos);
+        var dist2 = this.pos.dist(ghost2.pos);
+        if (!this.ImIT) {
+            if (ghost1.ImIT) {
+                //changed velocity multiplier to 3 and 5, giving quick and slow.
+                //secretly dependant on who is it, as that changes if the ghost is
+                //either the 1st one, or the 2nd to the "it" ghost's eyes
+                if (dist1 < 300) {
+                    this.velocity = p5.Vector.sub(ghost1.pos, this.pos);
+                    this.velocity.normalize();
+                    this.velocity.mult(5);
+                    this.velocity.rotate(PI); //without rotate, it goes toward "IT".
+                } else {
+                    this.fear();
+                }
+            } else if (ghost2.ImIT) {
+                if (dist2 < 300) {
+                    this.velocity = p5.Vector.sub(ghost2.pos, this.pos);
+                    this.velocity.normalize()
+                    this.velocity.mult(3)
+                    this.velocity.rotate(PI);
+                } else {
+                    this.fear();
+                }
+            } else {
+                this.fear();
+            }
+            this.pos.add(this.velocity);
+        };
+    }
+    this.fear = function() {
         this.velocity = p5.Vector.random2D()
-        this.velocity.mult(3)//shaking in fear!
+        this.velocity.mult(3) //shaking in fear!
+    }
 
-      }
+    this.attackA = function(ghost1, ghost2) { //the blue Ghost
+        if (this.ImIT) {
+            var dist1 = this.pos.dist(ghost1.pos);
+            var dist2 = this.pos.dist(ghost2.pos);
+            if (frameCount % 30 == 0) { //blue checks each second
+                if (dist1 < dist2) {
+                    this.velocity = p5.Vector.sub(ghost1.pos, this.pos);
+                    this.velocity.normalize();
+                    this.velocity.mult(4);
 
-      this.pos.add(this.velocity);
+                } else {
+                    this.velocity = p5.Vector.sub(ghost2.pos, this.pos);
+                    this.velocity.normalize();
+                    this.velocity.mult(4);
+                }
+            }
+            if (dist1 < this.size / 2) {
+                this.ImIT = false;
+                ghost1.ImIT = true;
+            }
+            if (dist2 < this.size / 2) {
+                this.ImIT = false;
+                ghost2.ImIT = true;
+            }
+            this.pos.add(this.velocity);
+        }
+    }
+    this.attackB = function(ghostB) {
+        if (this.ImIT) {
+            var distB = this.pos.dist(ghostB.pos);
+            if (frameCount % 60 == 0) { //red takes 1 second to process.
+                this.velocity = p5.Vector.sub(ghostB.pos, this.pos);
+                this.velocity.normalize(); //slows insane speed down
+                this.velocity.mult(4); //brings speed up minorly
+            }
+            if (distB < this.size / 2) {
+                this.ImIT = false;
+                ghostB.ImIT = true;
+            }
+            this.pos.add(this.velocity);
+        }
     }
 }
+
+
+
+
+
+
+
+
+//
+// this.movement = function() {
+//     var run = false
+//     for (var i = 0; i < ghosts.length; i++) {
+//         var test = ghosts[i];
+//         var ghostDist = p5.Vector.dist(test.pos, this.pos);
+//         console.log(ghostDist);
+//         if (ghostDist <= 45) {
+//             var run = true;
+//         }
+//     }
+//
+//     if (!this.ImIT) {
+//         if (run) {
+//             this.velocity = p5.Vector.random2D()
+//             this.velocity.mult(3) //shaking in fear!
+//         }
+//     }
+//
+//     this.pos.add(this.velocity);
+// }
--- Project 2/sketch.js
+++ Project 2/sketch.js
@@ -1,35 +1,45 @@
-var ghosts = [];
-
+var ghostA;
+var ghostB;
+var ghostC;
+var ghostArr = [];
+var whoIsIt = 0; //temporary mandating who is it. Remove and unhide setup floor
 
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    var whoIsIt = floor(random(2.99));
+    //var whoIsIt = floor(random(2.99));
+
 
     var bluGhost = '#4adfcb';
     var redGhost = '#ff3a10';
     var yelGhost = '#ffbe56';
-    var colArray = [bluGhost, redGhost, yelGhost];
+    ghostA = new Ghost(random(width), random(height), bluGhost)
+    ghostB = new Ghost(random(width), random(height), redGhost)
+    ghostC = new Ghost(random(width), random(height), yelGhost)
+    ghostArr = [ghostA, ghostB, ghostC];
+    //var colArray = [bluGhost, redGhost, yelGhost];
+    // for (var i = 0; i < 3; i++) {
+    //     ghosts[i] = new Ghost(random(0, width), random(0, height), colArray[i]);
+    // }
+    ghostArr[whoIsIt].ImIT = true;
 
-    for (var i = 0; i < 3; i++) {
-        ghosts[i] = new Ghosts(random(0, width), random(0, height), colArray[i]);
-    }
-    ghosts[whoIsIt].ImIT = true;
-    console.log(ghosts[whoIsIt].gCol);
 
 }
 
 function draw() {
+    console.log(ghostArr[whoIsIt].gCol);// add back into setup upon completion
     background(180);
     // ghosts.forEach(function(GhostTest, idx) {
     //     ghostTest.display();
     //     //ghosts[i].display();
     // });
-    for (var i = 0; i < ghosts.length; i++) {
-        ghosts[i].display();
-        ghosts[i].movement(ghosts);
-      }
-
+    for (var i = 0; i < ghostArr.length; i++) {
+        ghostArr[i].display();
+        ghostArr[i].walls();
+    }
+    ghostArr[0].moveA(ghostArr[1], ghostArr[2]);
+    ghostArr[1].moveB(ghostArr[0], ghostArr[2]);
+    ghostArr[2].moveC(ghostArr[0], ghostArr[1]);
 }
 
 //
